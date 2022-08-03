faa1313621cf281fd2841fe51d1a48f553fb920b
Александр <Volosnikov.aleksandr2@gmail.com>
2022-08-04 00:18:36 +0500

    Initial commit

---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using ArtistHelper.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArtistHelper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ImageMove(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+        private void Image_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                try
+                {
+                    foreach (var imagePath in files)
+                    {
+                        BitmapImage image = new(new Uri(imagePath));
+                        ImageWindows imgWin = new(image);
+                        imgWin.Show(); imgWin.Activate();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("ЧТот не получилось: " + ex);
+                    throw;
+                }
+
+            }
+        }
+    }
+
+}
