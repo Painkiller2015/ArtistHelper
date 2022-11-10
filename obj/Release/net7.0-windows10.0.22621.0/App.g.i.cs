61b05570338d9228a52af1c6655cca92b0dea209
Александр <Volosnikov.aleksandr2@gmail.com>
2022-11-10 23:19:42 +0500

    trash

--- obj/Release/net7.0-windows10.0.22621.0/App.g.i.cs
+++ obj/Release/net7.0-windows10.0.22621.0/App.g.i.cs
@@ -60,7 +60,7 @@ namespace ArtistHelper {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/ArtistHelper;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ArtistHelper;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- obj/Release/net7.0-windows10.0.22621.0/View/MainWindow.g.i.cs
+++ obj/Release/net7.0-windows10.0.22621.0/View/MainWindow.g.i.cs
@@ -62,7 +62,7 @@ namespace ArtistHelper {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ArtistHelper;component/view/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ArtistHelper;V1.0.0.0;component/view/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
