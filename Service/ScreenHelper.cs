24d55b433c0d6bab16974723407240071d84afdf
Александр <Volosnikov.aleksandr2@gmail.com>
2022-11-13 23:55:31 +0500

    Create getter multi Screen

---
+++ Service/ScreenHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows.Forms;
+using System.Windows;
+
+namespace ArtistHelper.Service
+{
+    public static class ScreenHelper
+    {
+        public static Screen CurrentScreen(this Window window)
+        {
+            return Screen.FromPoint(new System.Drawing.Point((int)window.Left, (int)window.Top));
+        }
+    }
+}
